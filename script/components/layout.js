3e6ae9daed765f666d6c602fb2cf15a8f87176eb
adrianarcag <adrianarcag@hotmail.com>
2022-05-23 13:54:50 +0200

    test11

--- script/components/layout.js
+++ script/components/layout.js
@@ -1,5 +1,24 @@
 function getNavigation() {
     return /*html*/ `
-    
+    <div class="topnav">
+         
+         
+    <img src="#">
+
+           <label class="switch">
+           <input type="checkbox">
+           <span class="slider round"></span>
+           </label>
+       
+           <span>Currency: </span>
+           <a onclick="render('inventory')">Inventory</a>
+           <a onclick="render('rewards')">Rewards</a>
+           <a  onclick="render('statistics')">Statistic</a>
+           <a  onclick="render('workouts')">Workout</a>
+           <a  onclick="render('currentWorkout')">Current</a>
+           <a  onclick="render('homepaget')">Home</a>
+           
+   
+</div>
     `;
 }
