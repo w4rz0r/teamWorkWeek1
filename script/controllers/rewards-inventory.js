ead1999afcca51b44e54dd5ad55b7132f447e900
w4rz0r <dani@getacademy.no>
2022-05-24 15:11:51 +0200

    rewards

--- script/controllers/rewards-inventory.js
+++ script/controllers/rewards-inventory.js
@@ -4,6 +4,24 @@ function moveRewardsToInventory(element) {
 
 }
 
+// function printRewards() {
+//     let rewardsHtml = "";
+//     for (let i = 0; i < model.data.rewards.length; i++) {
+//         rewardsHtml += `
+//         <div class="row">
+//         <div class="rewards-col">
+//             <img src="${model.data.rewards[i].image}">
+//             <div class="layer2">
+//                 <h3>${model.data.rewards[i].name}</h3>
+//             </div>
+//         </div>
+//     </div>
+//     `;
+//     }
+
+// }
+
+
 function printRewards() {
     let rewardsHtml = "";
     model.data.rewards.forEach(reward => {
