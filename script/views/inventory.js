12def4253aab3b314571a843a8867b2763eed28f
w4rz0r <dani@getacademy.no>
2022-06-02 13:35:03 +0200

    inventory

--- script/views/inventory.js
+++ script/views/inventory.js
@@ -2,85 +2,10 @@ views.inventory = () => {
     return /*html*/ `
     ${getNavigation()}
     <section class="inventory">
-    <h1 class="headLine">inventory</h1>
-    <div class="inventory-container">
-    <div class="inventory-row">
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>there</h3>
-            </div>
+        <h1 class="headLine">inventory</h1>
+        <div class="inventory-container">
+        ${printNftInventory()}
         </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>is</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>nothing</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>to</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>display</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>here</h3>
-            </div>
-        </div>
-    </div>
-    <div class="inventory-row">
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>i</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>am</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>empty</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>i</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>have</h3>
-            </div>
-        </div>
-        <div class="inventory-col">
-            <img src="">
-            <div class="layer2">
-                <h3>nothing</h3>
-            </div>
-        </div>
-    
-    </div>
 </section>
     `;
 }
