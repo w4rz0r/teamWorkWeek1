360f9aabd468d057b462fec3d5e3e6bca25ea98c
adrianarcag <adrianarcag@hotmail.com>
2022-06-07 12:19:25 +0200

    home page

--- script/model.js
+++ script/model.js
@@ -3,7 +3,7 @@ const model = {
     //app
     state:
     {
-        page: 'rewards',
+        page: 'homepage',
         silentMode: false,
         currentWorkoutId: null,
         currentUser: 8
