168276d010652c1569742670968e571c892aa78b
Viktorba91 <viktorbastnes@getacademy.no>
2022-06-08 13:27:22 +0200

    update userLogin

---
+++ script/views/userLogin.js
@@ -0,0 +1,22 @@
+views.userLogin = () => {
+    return /*html*/ `
+    
+    <div class="homepageText">
+    <div class="thp1">Welcome to AA-N </div>
+    
+    <div class="thp2">Your life is better when you use it!!</div>
+
+    <div class="thp3">Success is walking from failure to failure with no loss of enthusiasmt</div>
+
+    </div>
+    
+    <button onclick="loginUser(7)">kong Terje</button>
+    <button onclick="loginUser(8)">Benjamin</button>
+    <button onclick="loginUser(13)">aristoteles</button>
+    `;
+}
+
+function loginUser(loggedInAs){
+model.state.currentUser = loggedInAs;
+render('homepage');
+}
