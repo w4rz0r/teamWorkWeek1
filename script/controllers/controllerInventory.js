f83f65dba18883d7df4d5fb29d28677bbc9ef0a5
w4rz0r <dani@getacademy.no>
2022-06-27 10:25:24 +0200

    coupon update

--- script/controllers/controllerInventory.js
+++ script/controllers/controllerInventory.js
@@ -17,3 +17,20 @@ function printNftInventory() {
     };
 
 
+//printer ut cupong fra inventory til bruker til view
+function printCouponInventory() { 
+    let inventoryHtml = "";
+    user.inventory.forEach(nft => {
+        inventoryHtml += `
+        <div class="row">
+            <div class="rewards-col">
+                <img src="${nft.image}">
+                <div class="layer2">
+                    <h3>${nft.name}<br>$</h3>
+                </div>
+            </div>
+        </div>
+    `;
+    });
+    return inventoryHtml;
+};
