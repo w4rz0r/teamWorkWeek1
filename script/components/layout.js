9aa018a144cca802c952f29a246a893824ace9d9
adrianarcag <adrianarcag@hotmail.com>
2022-06-28 00:32:43 +0200

    cssChanges

--- script/components/layout.js
+++ script/components/layout.js
@@ -1,6 +1,7 @@
 function getNavigation() {
     return /*html*/ `
     <header>
+<<<<<<< Updated upstream
     ${user.name}
     <img class="logo" src="./images/Logo/logo2.svg" onclick="render('homepage')">
     
@@ -20,7 +21,34 @@ function getNavigation() {
     </nav>
     
     <span class="currency">Currency: ${user.currency}</span>
+=======
+        <div class="username-view">${user.name}</div>
+        <img class="logo" src="./images/Logo/logo2.svg" onclick="render('homepage')">
+                <label class="switch">
+                <input type="checkbox" ${model.state.silentModeisChecked} onchange="didToggle(this.checked)">
+                <span class="slider round"></span>
+                </label>
 
+
+>>>>>>> Stashed changes
+
+                <input type="checkbox" id="nav-toggle" class="nav-toggle">
+        <nav>
+            <ul>
+                <li><a onclick="render('homepage')">Home</a></li>
+                <li><a onclick="render('currentWorkout')">Current</a></li>
+                <li><a onclick="render('workouts')">Workouts</a></li>
+                <li><a onclick="render('statistics')">Statistic</a></li>
+                <li><a onclick="render('rewards')">Rewards</a></li>
+                <li><a onclick="render('inventory')">Inventory</a></li>
+            </ul>
+            
+        </nav>
+        <label for="nav-toggle" class="nav-toggle-label">
+        <span></span>
+        </label>
+        
+        <span class="currency-wallet"><ion-icon name="card-outline"></ion-icon>: ${user.currency}</span>
     </header>
     `;
 }
