270d3a1e79ebd1c8c129fe3b5adba51b0c961e23
w4rz0r <dani@getacademy.no>
2022-06-28 09:26:19 +0200

    navbar update

--- script/components/layout.js
+++ script/components/layout.js
@@ -1,27 +1,6 @@
 function getNavigation() {
     return /*html*/ `
     <header>
-<<<<<<< Updated upstream
-    ${user.name}
-    <img class="logo" src="./images/Logo/logo2.svg" onclick="render('homepage')">
-    
-            <label class="switch">
-            <input type="checkbox" ${model.state.silentModeisChecked} onchange="didToggle(this.checked)">
-            <span class="slider round"></span>
-            </label>
-    <nav>
-<ul class="nav__links">
-        <li><a onclick="render('homepage')">Home</a></li>
-        <li><a onclick="render('currentWorkout')">Current</a></li>
-        <li><a onclick="render('workouts')">Workouts</a></li>
-        <li><a onclick="render('statistics'), drawChart()">Statistic</a></li>
-        <li><a onclick="render('rewards')">Rewards</a></li>
-        <li><a onclick="render('inventory')">Inventory</a></li>
-</ul>
-    </nav>
-    
-    <span class="currency">Currency: ${user.currency}</span>
-=======
         <div class="username-view">${user.name}</div>
         <img class="logo" src="./images/Logo/logo2.svg" onclick="render('homepage')">
                 <label class="switch">
@@ -30,7 +9,6 @@ function getNavigation() {
                 </label>
 
 
->>>>>>> Stashed changes
 
                 <input type="checkbox" id="nav-toggle" class="nav-toggle">
         <nav>
